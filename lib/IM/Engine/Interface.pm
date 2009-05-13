b39d0b758e11493298ef3cd4659a8860a5aa0999
Shawn M Moore <sartak@gmail.com>
2009-05-13 01:42:47 -0400

    incoming -> outgoing loop

--- lib/IM/Engine/Interface.pm
+++ lib/IM/Engine/Interface.pm
@@ -14,6 +14,21 @@ has credentials => (
     auto_deref => 1,
 );
 
+sub received_message {
+    my $self     = shift;
+    my $incoming = shift;
+
+    return unless $self->has_incoming_callback;
+
+    my $outgoing = $self->incoming_callback->($incoming);
+
+    # Should we warn if $outgoing is true but not an Outgoing?
+    return unless blessed($outgoing)
+               && $outgoing->isa('IM::Engine::Outgoing');
+
+    $self->send_message($outgoing);
+}
+
 __PACKAGE__->meta->make_immutable;
 no Moose;
 
