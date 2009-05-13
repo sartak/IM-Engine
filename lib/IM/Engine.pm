f321df6fd4b270a8dec6c6529b37052b31a98f13
Shawn M Moore <sartak@gmail.com>
2009-05-13 03:33:10 -0400

    Synopsis

--- lib/IM/Engine.pm
+++ lib/IM/Engine.pm
@@ -33,3 +33,36 @@ no Moose::Util::TypeConstraints;
 
 1;
 
+__END__
+
+=head1 NAME
+
+IM::Engine - An HTTP::Engine for instant messaging
+
+=head1 SYNOPSIS
+
+    IM::Engine->new(
+        interface => {
+            protocol => 'AIM',
+            credentials => {
+                screenname => '...',
+                password   => '...',
+            },
+            incoming_callback => sub {
+                my $incoming = shift;
+
+                my $message = $incoming->message;
+                $message =~ tr[a-zA-Z][n-za-mN-ZA-M];
+
+                return $incoming->reply(
+                    message => $message,
+                );
+            },
+        },
+    )->run;
+
+=head1 DESCRIPTION
+
+
+=cut
+
