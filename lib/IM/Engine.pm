841f3e05d444938fccebb1d79deb6fe1539a9cab
Shawn M Moore <sartak@gmail.com>
2009-06-04 23:52:23 -0400

    pod stuff

--- lib/IM/Engine.pm
+++ lib/IM/Engine.pm
@@ -66,5 +66,16 @@ IM::Engine - The HTTP::Engine of instant messaging
 =head1 DESCRIPTION
 
 
+=head1 AUTHOR
+
+Shawn M Moore, C<sartak@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 Shawn M Moore.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
 =cut
 
