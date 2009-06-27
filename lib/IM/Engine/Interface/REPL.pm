fd686d103c3a3fca49ceec3153da5633a96036fa
Shawn M Moore <sartak@gmail.com>
2009-06-27 14:20:19 -0400

    Add a basic REPL interface

---
+++ lib/IM/Engine/Interface/REPL.pm
@@ -0,0 +1,38 @@
+package IM::Engine::Interface::REPL;
+use Moose;
+
+extends 'IM::Engine::Interface';
+
+sub send_message {
+    my $self     = shift;
+    my $outgoing = shift;
+
+    print $outgoing->message . "\n";
+}
+
+sub run {
+    my $self = shift;
+
+    while (1) {
+        my $input = do {
+            local $| = 1;
+            print "> ";
+            <STDIN>;
+        };
+        last if !defined($input);
+        chomp $input;
+
+        my $incoming = $self->incoming_class->new(
+            sender  => $self->user_class->new(name => $ENV{USER}),
+            message => $input,
+        );
+
+        $self->received_message($incoming);
+    }
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
