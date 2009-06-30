2394b9d229decc2f52777ffed7da6b63084bce74
Shawn M Moore <sartak@gmail.com>
2009-06-29 22:41:02 -0400

    Add CLI interface!

---
+++ lib/IM/Engine/Interface/CLI.pm
@@ -0,0 +1,30 @@
+package IM::Engine::Interface::CLI;
+use Moose;
+
+extends 'IM::Engine::Interface';
+
+sub send_message {
+    my $self     = shift;
+    my $outgoing = shift;
+
+    print $outgoing->message . "\n";
+}
+
+sub run {
+    my $self = shift;
+
+    my $input = join ' ', @ARGV;
+
+    my $incoming = $self->incoming_class->new(
+        sender  => $self->user_class->new(name => $ENV{USER}),
+        message => $input,
+    );
+
+    $self->received_message($incoming);
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
