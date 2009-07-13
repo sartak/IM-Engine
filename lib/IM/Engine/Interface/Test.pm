c1f7096d02ea0eef835e4d82aa10b48dac75d261
Shawn M Moore <sartak@gmail.com>
2009-07-12 20:07:29 -0400

    Begin adding a test interface

---
+++ lib/IM/Engine/Interface/Test.pm
@@ -0,0 +1,23 @@
+package IM::Engine::Interface::Test;
+use Moose;
+
+extends 'IM::Engine::Interface';
+
+has outgoing => (
+    metaclass => 'Collection::Array',
+    is        => 'ro',
+    isa       => 'ArrayRef[IM::Outgoing]',
+    provides  => {
+        push   => 'send_message',
+        splice => 'splice_outgoing',
+    },
+);
+
+sub run { confess "Do not call ->run in tests" }
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
+
