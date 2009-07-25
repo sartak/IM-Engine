fbe83e86d77f2e7542e7e90d0e7f0e95c599138f
Shawn M Moore <sartak@gmail.com>
2009-07-25 09:32:39 -0400

    HTMLish messages strip HTML from their plaintext

---
+++ lib/IM/Engine/Message/HTMLish.pm
@@ -0,0 +1,12 @@
+package IM::Engine::Message::HTMLish;
+use Moose::Role;
+use HTML::TreeBuilder;
+
+around _build_plaintext => sub {
+    my $orig = shift;
+    my $htmlish = $orig->(@_);
+    return HTML::TreeBuilder->new_from_content($htmlish)->as_text;
+};
+
+1;
+
