b95ddd5e45e0337508d5280c94c5e5ac13f04aa7
Shawn M Moore <sartak@gmail.com>
2009-08-08 20:49:36 -0400

    Doc for Incoming->reply

--- lib/IM/Engine/Incoming.pm
+++ lib/IM/Engine/Incoming.pm
@@ -48,3 +48,51 @@ __PACKAGE__->meta->make_immutable;
 no Moose;
 
 1;
+
+__END__
+
+=head1 NAME
+
+IM::Engine::Incoming
+
+=head1 ATTRIBUTES
+
+=head2 sender
+
+An instance of L<IM::Engine::User> which represents the sender of this incoming
+message.
+
+=head2 message
+
+See L<IM::Engine::Message/message>.
+
+=head2 plaintext
+
+See L<IM::Engine::Message/plaintext>.
+
+=head1 METHODS
+
+=head2 reply
+
+Constructs a L<IM::Engine::Outgoing> message that represents a reply to this
+incoming message.
+
+    my $outgoing = $incoming->reply("Sorry, I didn't understand.");
+
+You can also pass in a hash of attributes for constructing the outgoing
+message.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<IM::Engine::Incoming::IRC>
+
+=item L<IM::Engine::Incoming::Jabber>
+
+=item L<IM::Engine::Outgoing>
+
+=item L<IM::Engine::Message> (the superclass)
+
+=cut
+
