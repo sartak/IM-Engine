14c93e9a88fc31e2437fb884bcf442b33ba34af9
Shawn M Moore <sartak@gmail.com>
2009-08-08 21:31:04 -0400

    IM::Engine::User doc

--- lib/IM/Engine/User.pm
+++ lib/IM/Engine/User.pm
@@ -17,3 +17,38 @@ __PACKAGE__->meta->make_immutable;
 no Moose;
 
 1;
+
+__END__
+
+=head1 NAME
+
+IM::Engine::User
+
+=head1 ATTRIBUTES
+
+=head2 name
+
+The verbatim screenname (or nickname or jid or ...) of the user.
+
+=head1 METHODS
+
+=head2 canonical_name
+
+This method returns the name but with "arbitrary stuff" removed. This is more
+useful for storing the name in a database, or for comparing names.
+
+While documenting this method it gave me a bad feeling, so it very well may go
+away. C<:)>
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<IM::Engine::User::AIM>
+
+=item L<IM::Engine::User::Jabber>
+
+=back
+
+=cut
+
