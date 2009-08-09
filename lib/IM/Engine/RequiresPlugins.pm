c073623d6981a115404cd74e8913b0654d5e918d
Shawn M Moore <sartak@gmail.com>
2009-08-09 03:34:11 -0400

    RequiresPlugins role

---
+++ lib/IM/Engine/RequiresPlugins.pm
@@ -0,0 +1,26 @@
+package IM::Engine::RequiresPlugins;
+use MooseX::Role::Parameterized;
+
+parameter plugins => (
+    is       => 'ro',
+    isa      => 'ArrayRef[ClassName]|ClassName',
+    required => 1,
+);
+
+role {
+    my $p = shift;
+    my $plugins = $p->plugins;
+    my @plugins = ref($plugins) ? @$plugins : $plugins;
+
+    requires 'post_initialization', 'engine';
+
+    after post_initialization => sub {
+        my $self = shift;
+        my %has_plugin = map { blessed($_) => 1 } $self->engine->plugins;
+
+        for (@plugins) {
+            $has_plugin{$_} or die blessed($self) . " requires the $_ plugin.";
+        }
+    };
+};
+
