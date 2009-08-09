127bb91bf84e1c423ef8795035033ae8fb5f2637
Shawn M Moore <sartak@gmail.com>
2009-08-08 20:37:54 -0400

    IM::Engine::Message documentation

--- lib/IM/Engine/Message.pm
+++ lib/IM/Engine/Message.pm
@@ -24,3 +24,42 @@ __PACKAGE__->meta->make_immutable;
 no Moose;
 
 1;
+
+__END__
+
+=head1 NAME
+
+IM::Engine::Message
+
+=head1 SYNOPSIS
+
+    my $message = ...;
+
+    print $message->message;
+
+    print $message->plaintext;
+
+=head1 ATTRIBUTES
+
+=head2 message
+
+The exact message that this object encapsulates.
+
+=head2 plaintext
+
+The message that this object encapsulates but with any formatting stripped. For example, if this message represents an AIM IM, HTML will be stripped from the C<plaintext>.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<IM::Engine::Message::HTMLish>
+
+=item L<IM::Engine::Incoming>
+
+=item L<IM::Engine::Outgoing>
+
+=back
+
+=cut
+
